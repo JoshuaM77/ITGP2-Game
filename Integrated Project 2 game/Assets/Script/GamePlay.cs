81ea2cb430ae9c38b11d7a3f9666fa2d82c16fc6
JoshuaM77 <joshuamagee77@gmail.com>
2023-03-24 14:11:29 +0000

    woop

--- Integrated Project 2 game/Assets/Script/GamePlay.cs
+++ Integrated Project 2 game/Assets/Script/GamePlay.cs
@@ -11,9 +11,17 @@ public class GamePlay : MonoBehaviour
     public int OffenseOrDefense;
 
     public int Player1Health;
+    public int Player2Health;
+    public int EnemyHealth;
 
-    GameObject Player1Healthbar;
+    public GameObject Player1Healthbar;
 
+    public bool PlayerHadTurn = false;
+    public bool EnemyHadTurn = false;
+
+    public int AttackAmount;
+    public int HealAmount;
+    
     
 
     // Start is called before the first frame update
@@ -24,7 +32,11 @@ public class GamePlay : MonoBehaviour
         EnemyAlive = true;
         OffenseOrDefense = 0;
 
-        Player1Health = GetComponent<GameObject>(Player1Healthbar);
+        Player1Health = 100;
+        Player2Health = 100;
+        EnemyHealth = 100;
+        AttackAmount = 0;
+        HealAmount = 0;
 
         
     }
@@ -32,10 +44,24 @@ public class GamePlay : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+
+        
+        
+        
+
+        if (Player1Health > 0 || Player2Health > 0)
+        {
+            PlayerAlive = true;
+        }
+        
         while(PlayerAlive = true  && EnemyAlive == true)
         {
+            Debug.Log("Players Turn");
+            
             PlayersTurn();
-           
+
+            Debug.Log("Enemies Turn");
+
             EnemyTurn();
 
 
@@ -44,14 +70,42 @@ public class GamePlay : MonoBehaviour
 
     public void PlayersTurn()
     {
+        
+        
+        PlayerHadTurn = false;
 
+       
+
+    
     }
 
     
 
     public void EnemyTurn()
     {
+        
 
+        EnemyHadTurn = false;
+
+       
     }
+
+    public void RTPressed()
+    {
+        AttackAmount = Random.Range(5, 10);
+        Debug.Log("Attack amount is " + AttackAmount);
+        EnemyHealth = EnemyHealth - AttackAmount;
+        Debug.Log("Enemy has been hurt");
+        PlayerHadTurn = true;
+    }
+
+    public void LTPressed()
+    {
+        HealAmount = Random.Range(3, 8);
+        Player1Health = Player1Health + HealAmount;
+        Debug.Log("Player Has Been Healed");
+        PlayerHadTurn = true;
+    }
+
 
 }
